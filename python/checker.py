887a1a88fe8159f994bef520da35203707ec66dc
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-03-28 16:57:42 +0300

    initial commit for checker function

---
+++ python/checker.py
@@ -0,0 +1,56 @@
+import os
+import csv
+import re
+from alphabet_detector import AlphabetDetector
+
+
+def check_brackets(line):
+    if (line.count('[') == line.count(']') and line.count('[') == 3):
+        return True
+    return False
+
+
+def check_all_brackets(table):
+    for i, line in enumerate(table):
+        if not check_brackets(line):
+            raise Exception(f'Some issue with brackets in line {i + 1}')
+    else:
+        print('Brackets are checked')
+        return True
+
+
+def check_language(line):
+    ad = AlphabetDetector()
+    if (ad.only_alphabet_chars(line, "LATIN")):
+        return True
+    return False
+
+
+def check_all_language(table):
+    for i, line in enumerate(table):
+        if not check_language(line):
+            raise Exception(f'Some issues with language in line {i + 1}')
+    else:
+        print('Language is checked')
+        return True
+
+
+def extract_players(table):
+    players = []
+    for line in table:
+        player = {}
+        player['last_name'] = re.search('\[.*?\]', line).group(0)[1:-1].strip()
+        rb_index = line.index(']')
+        player['first_name'] = re.search(
+            '\[.*?\]', line[rb_index + 1:]).group(0)[1:-1].strip()
+        players.append(player)
+    return players
+
+
+location = os.path.split(__file__)[0]
+os.chdir(location)
+with open("../data/table.txt") as table:
+    table_body = table.readlines()[1:]
+    check_all_brackets(table_body)
+    check_all_language(table_body)
+    extract_players(table_body)
